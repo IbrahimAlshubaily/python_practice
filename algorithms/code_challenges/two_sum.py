21fc092a89b394aca9837b473542ca8ad507bd65
“IbrahimAlshubaily” <Ibra.94@live.com>
2022-05-19 19:21:46 +0300

    fix two sum lookup dictonary bug

--- algorithms/code_challenges/two_sum.py
+++ algorithms/code_challenges/two_sum.py
@@ -1,23 +1,25 @@
-#given a list and a number k, check if there are two elements in the list that sum to k:
-def two_sum_brute_force(nums , k):
-    for i in range(len(nums)-1):
-        for j in range(i+1, len(nums)):
+# given a list and a number k, check if there are two elements in the list that sum to k:
+def two_sum_brute_force(nums, k):
+    for i in range(len(nums) - 1):
+        for j in range(i + 1, len(nums)):
             if nums[i] + nums[j] == k:
                 return True
     return False
 
-def two_sum_sorted(nums , k):
+
+def two_sum_sorted(nums, k):
     nums.sort()
-    low, high = 0, len(nums)-1
+    low, high = 0, len(nums) - 1
     while low < high:
         curr_sum = nums[low] + nums[high]
         if curr_sum < k:
-            low+=1
+            low += 1
         elif curr_sum > k:
-            high-=1
+            high -= 1
         else:
             return True
     return False
+
 
 def two_sum_lookup(nums, k):
     lookup_table = {}
@@ -26,7 +28,7 @@ def two_sum_lookup(nums, k):
             lookup_table[nums[i]] = [i]
         elif len(lookup_table[nums[i]]) < 3:
             lookup_table[nums[i]] = lookup_table[nums[i]] + [i]
-        #else if the number exist more than twice, there is no need to track additional occurences.
+        # else if the number exist more than twice, there is no need to track additional occurrences.
     for i in range(len(nums)):
         if k - nums[i] in lookup_table:
             for index in lookup_table[k - nums[i]]:
@@ -42,14 +44,11 @@ def two_sum_complement_lookup(nums, k):
             return True
         complements_set.add(k - v)
     return False
-    
 
 
-
-
-k  = 17
+k = 17
 nums = [10, 5, 15, 3, 7, 7, 7]
-#print(two_sum_brute_force(nums , k))
-#print(two_sum_sorted(nums , k))
-#print(two_sum_lookup(nums, k))
+# print(two_sum_brute_force(nums , k))
+# print(two_sum_sorted(nums , k))
+# print(two_sum_lookup(nums, k))
 print(two_sum_complement_lookup(nums, k))
